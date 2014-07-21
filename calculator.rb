bc933dea1e7cd9e35794554f81afc0d0f38fc667
Jamie Crosby <jamescrosby123@gmail.com>
2014-07-20 18:15:08 -0700

    number validation

--- calculator.rb
+++ calculator.rb
@@ -1,18 +1,30 @@
 class Calculator
 
 	def add(num1, num2)
+		if num1.class != Fixnum or num2.class != Fixnum
+			return 'This calculator only works with whole numbers'
+		end
 		num1 + num2
 	end
 
 	def subtract(num1, num2)
+		if num1.class != Fixnum or num2.class != Fixnum
+			return 'This calculator only works with whole numbers'
+		end
 		num1 - num2
 	end
 
 	def multiply(num1, num2)
+		if num1.class != Fixnum or num2.class != Fixnum
+			return 'This calculator only works with whole numbers'
+		end
 		num1 * num2
 	end
 
 	def divide(num1, num2)
+		if num1.class != Fixnum or num2.class != Fixnum
+			return 'This calculator only works with whole numbers'
+		end
 		num1 / num2
 	end
 	
--- calculator_test.rb
+++ calculator_test.rb
@@ -7,21 +7,33 @@ class CalculatorTest < Test::Unit::TestCase
 	def test_add
 		calculator = Calculator.new
 		assert_equal(9, calculator.add(5, 4))
+		assert_nothing_raised do 
+			calculator.add(5, "test")
+		end
 	end
 
 	def test_subtract
 		calculator = Calculator.new
 		assert_equal(9, calculator.subtract(13, 4))
+		assert_nothing_raised do 
+			calculator.subtract("ruby", "test")
+		end
 	end
 
 	def test_multiply
 		calculator = Calculator.new
 		assert_equal(20, calculator.multiply(10, 2))
+		assert_nothing_raised do 
+			calculator.subtract("test", 5)
+		end
 	end
 
 	def test_divide
 		calculator = Calculator.new
 		assert_equal(20, calculator.divide(40, 2))
+		assert_nothing_raised do 
+			calculator.subtract(5, "test")
+		end
 	end
 	
 end
